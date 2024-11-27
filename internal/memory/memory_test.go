b537b7fc098e945cf246d25749b3ee63bb059cde
chnmk <a2molchanov@ya.ru>
2024-11-27 23:12:27 +0300

    test: add test templates for memory package

---
+++ internal/memory/memory_test.go
@@ -0,0 +1,85 @@
+package memory
+
+import "testing"
+
+func HandleValidMessage(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func HandleEmptyMessage(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func HandleRandomMessage(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func HandleUIDOnlyMessage(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ReadByIDInvalid(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ReadByIDValid(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ReadByUIDInvalid(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ReadByUIDValid(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ValidateInvalid(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ValidateValid(t *testing.T) {
+	var err error
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// Restore?
