86fd570c2476fab5355283fce7087c86069bb57f
chnmk <a2molchanov@ya.ru>
2024-11-29 00:09:41 +0300

    feat: add graceful http server shutdown

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"log/slog"
+	"net/http"
 	"os"
+	"sync"
 
 	cfg "github.com/chnmk/order-info-l0/internal/config"
-	"github.com/chnmk/order-info-l0/internal/consumer"
-	"github.com/chnmk/order-info-l0/internal/database"
-	"github.com/chnmk/order-info-l0/internal/memory"
+	"github.com/chnmk/order-info-l0/internal/transport"
 )
 
 func init() {
@@ -23,33 +26,56 @@ func init() {
 }
 
 func main() {
-	// Подключается к БД и создаёт отсутствующие таблицы.
-	cfg.DB = database.NewDB(cfg.DB, cfg.ExitCtx)
-	defer cfg.DB.Close()
-
-	// Инициализирует хранилище в памяти и восстанавливает данные из БД.
-	cfg.Data = memory.NewStorage(cfg.Data)
-	defer cfg.Exit()
-
-	// Проверяет подключение к Kafka.
-	consumer.Connect()
 
 	/*
-		// Запускает сервер.
-		http.HandleFunc("/orders", transport.GetOrder)
-		http.HandleFunc("/", transport.DisplayPage)
+		// Подключается к БД и создаёт отсутствующие таблицы.
+		cfg.DB = database.NewDB(cfg.DB, cfg.ExitCtx)
+		defer cfg.DB.Close()
 
-		slog.Info(
-			"starting server...",
-			"port", cfg.ServerPort,
-		)
+		// Инициализирует хранилище в памяти и восстанавливает данные из БД.
+		cfg.Data = memory.NewStorage(cfg.Data)
+		defer cfg.Exit()
 
-		err := http.ListenAndServe(fmt.Sprintf(":%s", cfg.ServerPort), nil)
-		if err != nil {
-			panic(err)
-		}
+		// Проверяет подключение к Kafka.
+		consumer.Connect()
 	*/
 
+	// Запускает сервер.
+	http.HandleFunc("/orders", transport.GetOrder)
+	http.HandleFunc("/", transport.DisplayPage)
+
+	server := &http.Server{Addr: fmt.Sprintf(":%s", cfg.ServerPort), Handler: nil}
+
+	slog.Info(
+		"starting server...",
+		"port", cfg.ServerPort,
+	)
+
+	var ServWg sync.WaitGroup
+	ServWg.Add(1)
+
+	go func() {
+		defer ServWg.Done()
+
+		cfg.ExitWg.Wait()
+		if err := server.Shutdown(cfg.ExitCtx); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		} else {
+			slog.Info("test")
+		}
+
+		slog.Info("server closed")
+	}()
+
+	err := server.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		slog.Error(
+			"shutdown error",
+			"err", err,
+		)
+	}
+
 	cfg.ExitWg.Wait()
+	ServWg.Wait()
 	slog.Info("shutting down...")
 }
